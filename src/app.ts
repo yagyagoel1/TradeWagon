cf13ecc50cfd11706d98cba904875716e5404e78
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-17 23:21:30 +0530

    added basic middleware setup with rate limiting cors policy contennt size restriction etc

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,38 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 const app = express();
+
+//middleware declaration
+app.use(limiter);
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, //to allow cookies from the client
+  })
+);
+app.disable("x-powered-by");
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+/*
+import userRouter from "./routes/user.route.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)*/
 
 export default app;
