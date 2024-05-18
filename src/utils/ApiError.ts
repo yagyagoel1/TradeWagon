a263a5edb866fcdeff3893f16b3e3603f36c8f4f
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-18 23:45:56 +0530

    made some changes in error template

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -11,7 +11,7 @@ class ApiError extends Error {
     errors = [],
     statck = ""
   ) {
-    super(message);
+    super();
     this.statusCode = statusCode;
     this.data = null;
     this.message = message;
