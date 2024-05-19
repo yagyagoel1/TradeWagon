bbd6893a80cd8242d39c755eebf81c63991c877e
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-19 16:21:49 +0530

    fixed bugs while sending mail

--- src/utils/jobs/sendEmail.job.ts
+++ src/utils/jobs/sendEmail.job.ts
@@ -6,7 +6,10 @@ import { otpMailTemplate } from "../../email/verificationEmailTemplate";
 
 export const emailQueueName = "emailQueue";
 const transporter = nodemailer.createTransport({
-  service: process.env.SERVICE as string,
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER as string,
     pass: process.env.MAIL_PASS as string,
@@ -27,7 +30,10 @@ export const handler = new Worker(
     //send otp to user
     const otp = await HandlingOtp(job.data);
     const mailOptions = {
-      from: process.env.MAIL_USER as string,
+      from: {
+        name: "Product Seller",
+        address: process.env.MAIL_USER as string,
+      },
       to: job.data.email,
       subject: "OTP for email verification",
       html: otpMailTemplate(job.data.fullName, otp.toString()),
@@ -39,8 +45,6 @@ export const handler = new Worker(
         console.log("Email sent: " + info.response);
       }
     });
-
-    console.log(job.data);
   },
   { connection: redisConnection }
 );
