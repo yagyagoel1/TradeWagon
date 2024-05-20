3c0c8091f18aaabfb612dfd32f085d8adbec6d7c
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-20 21:02:45 +0530

    edited the address schema

--- prisma/migrations/20240518063914_added_the_otp_shema/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "otp" TEXT,
-ADD COLUMN     "otpExpiry" TIMESTAMP(3);
--- prisma/migrations/20240520151345_added_address/migration.sql
+++ prisma/migrations/20240520151345_added_address/migration.sql
@@ -25,10 +25,9 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "fullName" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "avatar" TEXT,
+    "avatar" BOOLEAN NOT NULL DEFAULT false,
     "role" "Role" NOT NULL DEFAULT 'USER',
     "refreshToken" TEXT,
-    "accessToken" TEXT,
     "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -54,7 +53,7 @@ CREATE TABLE "Product" (
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
-    "image" TEXT NOT NULL,
+    "image" BOOLEAN NOT NULL DEFAULT false,
     "rating" DOUBLE PRECISION NOT NULL DEFAULT 0,
     "purchaseCount" INTEGER NOT NULL DEFAULT 0,
     "productType" "ProductType" NOT NULL,
@@ -116,6 +115,16 @@ CREATE TABLE "Address" (
 );
 
 -- CreateTable
+CREATE TABLE "Otp" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "_CartToProduct" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
@@ -135,6 +144,12 @@ CREATE UNIQUE INDEX "Cart_ownerEmail_key" ON "Cart"("ownerEmail");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Transaction_orderId_key" ON "Transaction"("orderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_ownerEmail_key" ON "Address"("ownerEmail");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Otp_userEmail_key" ON "Otp"("userEmail");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_CartToProduct_AB_unique" ON "_CartToProduct"("A", "B");
@@ -168,6 +183,9 @@ ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_orderId_fkey" FOREIGN KEY 
 
 -- AddForeignKey
 ALTER TABLE "Address" ADD CONSTRAINT "Address_ownerEmail_fkey" FOREIGN KEY ("ownerEmail") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Otp" ADD CONSTRAINT "Otp_userEmail_fkey" FOREIGN KEY ("userEmail") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_CartToProduct" ADD CONSTRAINT "_CartToProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
