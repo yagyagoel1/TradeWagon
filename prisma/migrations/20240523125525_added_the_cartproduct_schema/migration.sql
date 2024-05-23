fef4c098d88c6d528a184519485e33c5c4a8dffc
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-23 18:26:05 +0530

    updated the schema added the cart product schemaa

---
+++ prisma/migrations/20240523125525_added_the_cartproduct_schema/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_CartToProduct` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "_CartToProduct" DROP CONSTRAINT "_CartToProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_CartToProduct" DROP CONSTRAINT "_CartToProduct_B_fkey";
+
+-- DropTable
+DROP TABLE "_CartToProduct";
+
+-- CreateTable
+CREATE TABLE "CartProduct" (
+    "id" TEXT NOT NULL,
+    "cartId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CartProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CartProduct" ADD CONSTRAINT "CartProduct_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartProduct" ADD CONSTRAINT "CartProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
